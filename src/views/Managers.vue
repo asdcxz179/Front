501faacc8000871ae497b089d0783256af9f3cc6
Henry <asdcxz179@gmail.com>
2020-09-30 08:41:31 +0800

    ad

--- src/views/Managers.vue
+++ src/views/Managers.vue
@@ -16,6 +16,7 @@
           :items="desserts"
           :items-per-page="10"
           class="elevation-1"
+          :loading="loading" v-bind:loading-text="$t('common.loading')"
         ></v-data-table>
       </v-col>
     </v-row>
@@ -26,6 +27,9 @@
   export default {
     data () {
       return {
+        loading:true,
+        totalDesserts:0,
+        options:{},
         headers: [
           {
             text: this.$i18n.t('common.no'),
@@ -37,91 +41,23 @@
           { text: this.$i18n.t('common.name'), value: 'name' },
           { text: this.$i18n.t('common.email'), value: 'email' },
           { text: this.$i18n.t('common.created_at'), value: 'created_at' },
-          { text: 'Iron (%)', value: 'iron' },
         ],
         desserts: [
-          {
-            name: 'Frozen Yogurt',
-            calories: 159,
-            fat: 6.0,
-            carbs: 24,
-            protein: 4.0,
-            iron: '1%',
-          },
-          {
-            name: 'Ice cream sandwich',
-            calories: 237,
-            fat: 9.0,
-            carbs: 37,
-            protein: 4.3,
-            iron: '1%',
-          },
-          {
-            name: 'Eclair',
-            calories: 262,
-            fat: 16.0,
-            carbs: 23,
-            protein: 6.0,
-            iron: '7%',
-          },
-          {
-            name: 'Cupcake',
-            calories: 305,
-            fat: 3.7,
-            carbs: 67,
-            protein: 4.3,
-            iron: '8%',
-          },
-          {
-            name: 'Gingerbread',
-            calories: 356,
-            fat: 16.0,
-            carbs: 49,
-            protein: 3.9,
-            iron: '16%',
-          },
-          {
-            name: 'Jelly bean',
-            calories: 375,
-            fat: 0.0,
-            carbs: 94,
-            protein: 0.0,
-            iron: '0%',
-          },
-          {
-            name: 'Lollipop',
-            calories: 392,
-            fat: 0.2,
-            carbs: 98,
-            protein: 0,
-            iron: '2%',
-          },
-          {
-            name: 'Honeycomb',
-            calories: 408,
-            fat: 3.2,
-            carbs: 87,
-            protein: 6.5,
-            iron: '45%',
-          },
-          {
-            name: 'Donut',
-            calories: 452,
-            fat: 25.0,
-            carbs: 51,
-            protein: 4.9,
-            iron: '22%',
-          },
-          {
-            name: 'KitKat',
-            calories: 518,
-            fat: 26.0,
-            carbs: 65,
-            protein: 7,
-            iron: '6%',
-          },
         ],
       }
     },
+    created:function(){
+      this.GetManager();
+    },
+    methods:{
+      GetManager(){
+        this.$axios.get('/api/v1/Manager').then((res)=>{
+          if(res.data.status=='success'){
+            this.desserts   = res.data.data.original.data;
+          }
+          this.loading  = false;
+        });
+      }
+    }
   }
 </script>
