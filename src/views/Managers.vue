658e97fc2ee74b5af9ade3538d032bf92b55828d
Henry <asdcxz179@gmail.com>
2020-12-09 19:27:09 +0800

    fiix

--- src/views/Managers.vue
+++ src/views/Managers.vue
@@ -137,195 +137,197 @@
                   </v-form>
                 </ValidationObserver>
               </v-dialog>
-            </v-toolbar>
-          </template>
-          <template v-slot:item.uuid="{ item }">
-            <v-dialog v-model="edit_dialog" max-width="500px" :retain-focus="false">
-              <template #activator="{ on: dialog }">
-                <v-tooltip bottom >
-                  <template #activator="{ on: tooltip }">
-                    <v-icon
-                      small
-                      class="mr-2"
-                      @click="GetManagerDetail(item.uuid)"
-                      v-on="{ ...tooltip, ...dialog }"
-                    >
-                      mdi-pencil
-                    </v-icon>
-                  </template>
-                  <span>{{$t('managers-page.edit-manger')}}</span>
-                </v-tooltip>
-              </template>
-              <ValidationObserver ref="EditForm" >
-                <v-form @submit="EditManager" ref="edit_form">
-                  <v-card>
-                    <v-card-title>
-                      <span class="headline">{{$t('managers-page.edit-manger')}}</span>
-                    </v-card-title>
-                    <v-card-text>
-                      <v-container>
-                        <v-row>
-                          <v-col cols="12">
-                            <ValidationProvider v-slot="{ errors }" v-bind:name="$t('common.account')" rules="min:6|max:12">
-                              <v-text-field 
-                                v-bind:label="$t('common.account')" 
-                                type="text" 
-                                :error-messages="errors"
-                                v-model="EditForm.username"
-                                :disabled="true"
-                              ></v-text-field>
-                            </ValidationProvider>
-                          </v-col>
-                          <v-col cols="12">
-                            <ValidationProvider v-slot="{ errors }" v-bind:name="$t('common.password')" rules="min:6|max:12">
-                              <v-text-field 
-                                v-bind:label="$t('common.password')" 
-                                type="password" 
-                                v-model="EditForm.password"
-                                :error-messages="errors"
-                              ></v-text-field>
-                            </ValidationProvider>
-                          </v-col>
-                          <v-col cols="12">
-                            <ValidationProvider v-slot="{ errors }" v-bind:name="$t('common.password_confirmation')" rules="min:6|max:12">
-                              <v-text-field 
-                                v-bind:label="$t('common.password_confirmation')" 
-                                type="password" 
-                                v-model="EditForm.password_confirmation"
-                                :error-messages="errors"
-                              ></v-text-field>
-                            </ValidationProvider>
-                          </v-col>
-                          <v-col cols="12">
-                            <ValidationProvider v-slot="{ errors }" v-bind:name="$t('common.name')" rules="required|min:3|max:20">
-                              <v-text-field 
-                                v-bind:label="$t('common.name')" 
-                                type="text" 
-                                required 
-                                v-model="EditForm.name"
-                                :error-messages="errors"
-                              ></v-text-field>
-                            </ValidationProvider>
-                          </v-col>
-                          <v-col cols="12">
-                            <ValidationProvider v-slot="{ errors }" v-bind:name="$t('common.email')" rules="email">
-                              <v-text-field 
-                                v-bind:label="$t('common.email')" 
-                                type="text" 
-                                required 
-                                v-model="EditForm.email"
-                                :error-messages="errors"
-                                :disabled="true"
-                              ></v-text-field>
-                            </ValidationProvider>
-                          </v-col>
-                          <v-col cols="12">
-                            <ValidationProvider v-slot="{ errors }" v-bind:name="$t('common.group')" rules="required">
-                              <v-select 
-                                v-bind:label="$t('common.group')" 
-                                required 
-                                v-model="EditForm.group"
-                                :error-messages="errors"
-                                :items="GroupItems"
-                              ></v-select>
-                            </ValidationProvider>
-                          </v-col>
-                          <v-col cols="12">
-                            <ValidationProvider v-slot="{ errors }" v-bind:name="$t('common.role')" rules="required">
-                              <v-select 
-                                v-bind:label="$t('common.role')" 
-                                required 
-                                v-model="EditForm.role"
-                                :error-messages="errors"
-                                :items="RoleItems"
-                              ></v-select>
-                            </ValidationProvider>
-                          </v-col>
-                        </v-row>
-                      </v-container>
-                    </v-card-text>
-                    <v-card-actions>
-                      <v-spacer></v-spacer>
-                      <v-btn color="blue darken-1" text @click="edit_dialog=false">{{$t('common.cancel')}}</v-btn>
-                      <v-btn color="blue darken-1" text type="submit">{{$t('common.update')}}</v-btn>
-                    </v-card-actions>
-                  </v-card>
-                </v-form>
-              </ValidationObserver>
-            </v-dialog>
-            <v-dialog v-model="disable_dialog" max-width="500px" :retain-focus="false">
-              <template #activator="{ on: dialog }">
-                <v-tooltip bottom >
-                  <template #activator="{ on: tooltip }">
-                    <v-icon
-                      small
-                      v-on="{ ...tooltip, ...dialog }"
-                      v-if="item.status==1"
-                    >
-                      mdi-block-helper
-                    </v-icon>
-                    <v-icon
-                      small
-                      v-on="{ ...tooltip, ...dialog }"
-                      v-else
-                    >
-                      mdi-check
-                    </v-icon>
-                  </template>
-                  <span
-                    v-if="item.status==1"
+              <v-dialog v-model="edit_dialog" max-width="500px" :retain-focus="false">
+                <ValidationObserver ref="EditForm" >
+                  <v-form @submit="EditManager" ref="edit_form">
+                    <v-card>
+                      <v-card-title>
+                        <span class="headline">{{$t('managers-page.edit-manger')}}</span>
+                      </v-card-title>
+                      <v-card-text>
+                        <v-container>
+                          <v-row>
+                            <v-col cols="12">
+                              <ValidationProvider v-slot="{ errors }" v-bind:name="$t('common.account')" rules="min:6|max:12">
+                                <v-text-field 
+                                  v-bind:label="$t('common.account')" 
+                                  type="text" 
+                                  :error-messages="errors"
+                                  v-model="EditForm.username"
+                                  :disabled="true"
+                                ></v-text-field>
+                              </ValidationProvider>
+                            </v-col>
+                            <v-col cols="12">
+                              <ValidationProvider v-slot="{ errors }" v-bind:name="$t('common.password')" rules="min:6|max:12">
+                                <v-text-field 
+                                  v-bind:label="$t('common.password')" 
+                                  type="password" 
+                                  v-model="EditForm.password"
+                                  :error-messages="errors"
+                                ></v-text-field>
+                              </ValidationProvider>
+                            </v-col>
+                            <v-col cols="12">
+                              <ValidationProvider v-slot="{ errors }" v-bind:name="$t('common.password_confirmation')" rules="min:6|max:12">
+                                <v-text-field 
+                                  v-bind:label="$t('common.password_confirmation')" 
+                                  type="password" 
+                                  v-model="EditForm.password_confirmation"
+                                  :error-messages="errors"
+                                ></v-text-field>
+                              </ValidationProvider>
+                            </v-col>
+                            <v-col cols="12">
+                              <ValidationProvider v-slot="{ errors }" v-bind:name="$t('common.name')" rules="required|min:3|max:20">
+                                <v-text-field 
+                                  v-bind:label="$t('common.name')" 
+                                  type="text" 
+                                  required 
+                                  v-model="EditForm.name"
+                                  :error-messages="errors"
+                                ></v-text-field>
+                              </ValidationProvider>
+                            </v-col>
+                            <v-col cols="12">
+                              <ValidationProvider v-slot="{ errors }" v-bind:name="$t('common.email')" rules="email">
+                                <v-text-field 
+                                  v-bind:label="$t('common.email')" 
+                                  type="text" 
+                                  required 
+                                  v-model="EditForm.email"
+                                  :error-messages="errors"
+                                  :disabled="true"
+                                ></v-text-field>
+                              </ValidationProvider>
+                            </v-col>
+                            <v-col cols="12">
+                              <ValidationProvider v-slot="{ errors }" v-bind:name="$t('common.group')" rules="required">
+                                <v-select 
+                                  v-bind:label="$t('common.group')" 
+                                  required 
+                                  v-model="EditForm.group"
+                                  :error-messages="errors"
+                                  :items="GroupItems"
+                                ></v-select>
+                              </ValidationProvider>
+                            </v-col>
+                            <v-col cols="12">
+                              <ValidationProvider v-slot="{ errors }" v-bind:name="$t('common.role')" rules="required">
+                                <v-select 
+                                  v-bind:label="$t('common.role')" 
+                                  required 
+                                  v-model="EditForm.role"
+                                  :error-messages="errors"
+                                  :items="RoleItems"
+                                ></v-select>
+                              </ValidationProvider>
+                            </v-col>
+                          </v-row>
+                        </v-container>
+                      </v-card-text>
+                      <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn color="blue darken-1" text @click="edit_dialog=false">{{$t('common.cancel')}}</v-btn>
+                        <v-btn color="blue darken-1" text type="submit">{{$t('common.update')}}</v-btn>
+                      </v-card-actions>
+                    </v-card>
+                  </v-form>
+                </ValidationObserver>
+              </v-dialog>
+              <v-dialog v-model="disable_dialog" max-width="500px" :retain-focus="false">
+                <v-card>
+                  <v-card-title 
+                    class="headline"
+                    v-if="DisableStatus==0"
                   >
                     {{$t('managers-page.disable-manger')}}
-                  </span>
-                  <span
+                  </v-card-title>
+                   <v-card-title 
+                    class="headline"
                     v-else
                   >
                     {{$t('managers-page.enable-manger')}}
-                  </span>
-                </v-tooltip>
+                  </v-card-title>
+                  <v-card-text
+                    v-if="DisableStatus==0"
+                  >
+                    {{$t('common.are-you-disable')}}
+                  </v-card-text>
+                  <v-card-text
+                    v-else
+                  >
+                    {{$t('common.are-you-enable')}}
+                  </v-card-text>
+                  <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn
+                      color="blue darken-1"
+                      text
+                      @click="disable_dialog= false"
+                    >
+                      {{$t('common.cancel')}}
+                    </v-btn>
+                    <v-btn
+                      color="blue darken-1"
+                      text
+                      @click="DisableManager()"
+                    >
+                      {{$t('common.confirm')}}
+                    </v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-dialog>
+            </v-toolbar>
+          </template>
+          <template v-slot:item.uuid="{ item }">
+            <v-tooltip bottom >
+              <template #activator="{ on: tooltip }">
+                <v-icon
+                  small
+                  class="mr-2"
+                  @click="GetManagerDetail(item.uuid)"
+                  v-on="{ ...tooltip}"
+                >
+                  mdi-pencil
+                </v-icon>
               </template>
-              <v-card>
-                <v-card-title 
-                  class="headline"
+              <span>{{$t('managers-page.edit-manger')}}</span>
+            </v-tooltip>
+            <v-tooltip bottom >
+              <template #activator="{ on: tooltip }">
+                <v-icon
+                  small
+                  class="mr-2"
+                  v-on="{ ...tooltip}"
+                  @click="DisableItem(item)"
                   v-if="item.status==1"
                 >
-                  {{$t('managers-page.disable-manger')}}
-                </v-card-title>
-                 <v-card-title 
-                  class="headline"
+                  mdi-block-helper
+                </v-icon>
+                <v-icon
+                  small
+                  class="mr-2"
+                  @click="DisableItem(item)"
+                  v-on="{ ...tooltip}"
                   v-else
                 >
-                  {{$t('managers-page.enable-manger')}}
-                </v-card-title>
-                <v-card-text
-                  v-if="item.status==1"
-                >
-                  {{$t('common.are-you-disable')}}
-                </v-card-text>
-                <v-card-text
-                  v-else
-                >
-                  {{$t('common.are-you-enable')}}
-                </v-card-text>
-                <v-card-actions>
-                  <v-spacer></v-spacer>
-                  <v-btn
-                    color="blue darken-1"
-                    text
-                    @click="disable_dialog= false"
-                  >
-                    {{$t('common.cancel')}}
-                  </v-btn>
-                  <v-btn
-                    color="blue darken-1"
-                    text
-                    @click="DisableItem(item)"
-                  >
-                    {{$t('common.confirm')}}
-                  </v-btn>
-                </v-card-actions>
-              </v-card>
-            </v-dialog>
+                  mdi-check
+                </v-icon>
+              </template>
+              <span
+                v-if="item.status==1"
+              >
+                {{$t('managers-page.disable-manger')}}
+              </span>
+              <span
+                v-else
+              >
+                {{$t('managers-page.enable-manger')}}
+              </span>
+            </v-tooltip>
+            
+            
           </template>
         </v-data-table>
       </v-col>
@@ -344,6 +346,8 @@
         GroupItems:[],
         RoleItems:[],
         EditUUid:"",
+        DisableUUid:"",
+        DisableStatus:1,
         options: {
           page: 1,
           itemsPerPage:10,
@@ -402,12 +406,6 @@
         },
         deep: true,
       },
-      edit_dialog(){
-        if(typeof(this.$refs.edit_form)!=='undefined'){
-          this.$refs.edit_form.reset();
-          this.$refs.EditForm.reset();  
-        }
-      },
       add_dialog(){
         if(typeof(this.$refs.add_form)!=='undefined'){
           this.$refs.add_form.reset();
@@ -454,6 +452,10 @@
       },
       GetManagerDetail(uuid){
         this.EditUUid   = uuid;
+        if(typeof(this.$refs.edit_form)!=='undefined'){
+          this.$refs.edit_form.reset();
+          this.$refs.EditForm.reset();  
+        }
         this.$axios.get('/api/v1/Manager/'+this.EditUUid).then((res)=>{
             if(res.data.status=='success'){
               var tmp   = this.EditForm;
@@ -490,15 +492,18 @@
                   this.$common.AxiosHandle(res);
                   this.edit_dialog=false;
                   this.GetManager();
-
                 }
             });    
           }
         })
       },
       DisableItem(item){
-        let status  =   (item.status)?0:1;
-        this.$axios.put('/api/v1/DisableManager/'+item.uuid,{status:status}).then((res)=>{
+        this.disable_dialog = true;
+        this.DisableUUid  = item.uuid;
+        this.DisableStatus   = (item.status)?0:1;
+      },
+      DisableManager(){
+        this.$axios.put('/api/v1/DisableManager/'+this.DisableUUid,{status:this.DisableStatus}).then((res)=>{
           if(res.data.status=='success'){
             this.$common.AxiosHandle(res);
             this.disable_dialog=false;
